297481c5edd42d5284c3bee628aa5a23b4409bba
alexandruradu89 <89183567+alexandruradu89@users.noreply.github.com>
2024-01-13 18:54:23 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -1,32 +1,25 @@
 function setWinnerButton() {
     let noButtons = document.getElementById("noButtons").value;
     let winner = Math.floor(Math.random() * noButtons) + 1;
-    console.log(winner);
     return winner;
-}
-
-function revealButton(winner, buttonNo, button) {
-    if (buttonNo === winner) {
-        button.innerHTML = "winner";
-    } else {
-        button.innerHTML = "loser";
-    }
 }
 
 function generateButtons() {
     let winner = setWinnerButton();
     let noButtons = document.getElementById("noButtons").value;
     for (var i = 1; i <= noButtons; ++i) {
-        let div = document.createElement("div");
-        document.body.appendChild(div);
-
         let button = document.createElement("button");
-        button.id = i;
-        console.log(button.id);
-        //button.style="--bs-btn-padding-y: 1rem; --bs-btn-padding-x: 1rem; --bs-btn-font-size: .75rem;"
+        button.Id = i;
+        button.textContent = "Button " + i;
         button.className = "btn btn-success";
-        //button.onclick = revealButton(winner, i, button);
-        div.appendChild(button);
+        button.addEventListener("click", function() {
+            if (winner == button.Id) {
+                alert("Winner");
+            } else {
+                alert("Loser");
+            }
+        });
+        generateButtonsDiv.appendChild(button);
 
     }
 }
